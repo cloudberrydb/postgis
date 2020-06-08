ba3cd22f06e9ff3f726bb165e390e63061e3c604
Ekta Khanna <ekhanna@pivotal.io>
2020-06-08 12:19:01 -0700

    Update script for install in schema

--- postgis/package/postgis_manager.sh
+++ postgis/package/postgis_manager.sh
@@ -23,7 +23,7 @@ if [ "$#" -eq 2 ]
 	fi
 elif [ "$#" -eq 3 ]
 then
-	is_schema_in_db=`psql -t -d $1 -c "SELECT schema_name FROM information_schema.schemata WHERE schema_name='$2';" | HEAD -n 1`
+	is_schema_in_db=`psql -t -d $1 -c "SELECT schema_name FROM information_schema.schemata WHERE schema_name='$2';" | head -n 1`
 	if [ -z $is_schema_in_db ]
 	then
 		echo "Schema('$2') does not exist in database('$1')"
@@ -31,23 +31,23 @@ then
 	fi
 	if [ "$3" = "install" ]
 	then
-		env PGOPTIONS='--search_path=$2' psql --no-psqlrc -d $1 -f $GPHOME/share/postgresql/contrib/postgis-2.5/install/postgis.sql;
-		env PGOPTIONS='--search_path=$2' psql --no-psqlrc -d $1 -f $GPHOME/share/postgresql/contrib/postgis-2.5/install/rtpostgis.sql;
-		env PGOPTIONS='--search_path=$2' psql --no-psqlrc -d $1 -f $GPHOME/share/postgresql/contrib/postgis-2.5/install/postgis_comments.sql;
-		env PGOPTIONS='--search_path=$2' psql --no-psqlrc -d $1 -f $GPHOME/share/postgresql/contrib/postgis-2.5/install/raster_comments.sql;
-		env PGOPTIONS='--search_path=$2' psql --no-psqlrc -d $1 -f $GPHOME/share/postgresql/contrib/postgis-2.5/install/spatial_ref_sys.sql;
+		env PGOPTIONS="--search_path=$2" psql --no-psqlrc -d $1 -f $GPHOME/share/postgresql/contrib/postgis-2.5/install/postgis.sql;
+		env PGOPTIONS="--search_path=$2" psql --no-psqlrc -d $1 -f $GPHOME/share/postgresql/contrib/postgis-2.5/install/rtpostgis.sql;
+		env PGOPTIONS="--search_path=$2" psql --no-psqlrc -d $1 -f $GPHOME/share/postgresql/contrib/postgis-2.5/install/postgis_comments.sql;
+		env PGOPTIONS="--search_path=$2" psql --no-psqlrc -d $1 -f $GPHOME/share/postgresql/contrib/postgis-2.5/install/raster_comments.sql;
+		env PGOPTIONS="--search_path=$2" psql --no-psqlrc -d $1 -f $GPHOME/share/postgresql/contrib/postgis-2.5/install/spatial_ref_sys.sql;
 	elif [ "$3" = "upgrade" ]
 	then
-		env PGOPTIONS='--search_path=$2' psql --no-psqlrc -d $1 -f $GPHOME/share/postgresql/contrib/postgis-2.5/upgrade/legacy.sql;
-		env PGOPTIONS='--search_path=$2' psql --no-psqlrc -d $1 -f $GPHOME/share/postgresql/contrib/postgis-2.5/upgrade/postgis_upgrade_20_21.sql;
-		env PGOPTIONS='--search_path=$2' psql --no-psqlrc -d $1 -f $GPHOME/share/postgresql/contrib/postgis-2.5/upgrade/postgis_upgrade_21_minor.sql;
+		env PGOPTIONS="--search_path=$2" psql --no-psqlrc -d $1 -f $GPHOME/share/postgresql/contrib/postgis-2.5/upgrade/legacy.sql;
+		env PGOPTIONS="--search_path=$2" psql --no-psqlrc -d $1 -f $GPHOME/share/postgresql/contrib/postgis-2.5/upgrade/postgis_upgrade_20_21.sql;
+		env PGOPTIONS="--search_path=$2" psql --no-psqlrc -d $1 -f $GPHOME/share/postgresql/contrib/postgis-2.5/upgrade/postgis_upgrade_21_minor.sql;
 	elif [ "$3" = "uninstall" ]
 	then
-		env PGOPTIONS='--search_path=$2' psql --no-psqlrc -d $1 -f $GPHOME/share/postgresql/contrib/postgis-2.5/uninstall/uninstall_rtpostgis.sql;
-		env PGOPTIONS='--search_path=$2' psql --no-psqlrc -d $1 -f $GPHOME/share/postgresql/contrib/postgis-2.5/uninstall/uninstall_postgis.sql;
+		env PGOPTIONS="--search_path=$2" psql --no-psqlrc -d $1 -f $GPHOME/share/postgresql/contrib/postgis-2.5/uninstall/uninstall_rtpostgis.sql;
+		env PGOPTIONS="--search_path=$2" psql --no-psqlrc -d $1 -f $GPHOME/share/postgresql/contrib/postgis-2.5/uninstall/uninstall_postgis.sql;
 	else
 		echo "Invalid option. Please try install, upgrade or uninstall"
 	fi
 else
-	echo "Invalid arguements. Usage: ./postgis_manager.sh <dbname> [<schema_name>] install/uninstall/upgrade "
+	echo "Invalid arguements. Usage: ./postgis_manager.sh <dbname> [<schema_name>] install/uninstall "
 fi
